1151127b80cc9cfc095df8632214eb2d2de26ca4
Alexander Lukyanov <lux-nova@mail.ru>
2021-11-13 22:18:59 +0300

    like a spring

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,24 +1,23 @@
 package ru.netology.controller;
 
 import com.google.gson.Gson;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.Reader;
+import java.util.List;
 
 /**
  * Предоставляет методы, соответствующие функционалу сервера,
  * осуществляет трансформацию представления данных между форматом запроса
  * и форматом хранения-обработки и записывает данные в ответ.
  */
-@Controller
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
-    public static final String APPLICATION_JSON = "application/json";
-    public static final String TEXT_PLAIN = "text/plain";
-    private static final Gson converter = new Gson();
+//    public static final String APPLICATION_JSON = "application/json";
+//    public static final String TEXT_PLAIN = "text/plain";
+//    private static final Gson converter = new Gson();
 
     private final PostService service;
 
@@ -31,53 +30,36 @@ public class PostController {
     }
 
     /**
-     * Записывает в ответ список постов, полученный от сервиса.
-     * @param response ответ, в который будет записан список всех доступных постов.
-     * @throws IOException при проблемах со связью.
+     * Отдаёт в ответ список постов, полученный от сервиса.
      */
-    public void all(HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var data = service.all();
-        response.getWriter().print(converter.toJson(data));
+    @GetMapping
+    public List<Post> all() {
+        return service.all();
     }
 
     /**
-     * Записывает в ответ пост с запрошенным номером, получив его от сервиса (если не будет прервано исключением).
+     * Отдаёт в ответ пост с запрошенным номером, получив его от сервиса (если не будет прервано исключением).
      * @param id       запрашиваемый номер поста.
-     * @param response ответ, в который будет записан найденный пост.
-     * @throws IOException при проблемах со связью.
      */
-    public void getById(long id, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var data = service.getById(id);
-        response.getWriter().print(converter.toJson(data));
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id) {
+        return service.getById(id);
     }
 
     /**
      * Передаёт сервису полученный пост на сохранение, получает его обратно
-     * в сохранённом виде и записывает в ответ.
-     * @param body     входной поток, трактуемый как тело поста.
-     * @param response ответ, в который будет записан сохранённый пост.
-     * @throws IOException при проблемах со связью.
+     * в сохранённом виде и отдаёт в ответ.
      */
-    public void save(Reader body, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var post = converter.fromJson(body, Post.class);
-        final var data = service.save(post);
-        response.getWriter().print(converter.toJson(data));
+    @PostMapping
+    public Post save(@RequestBody Post post) {
+        return service.save(post);
     }
 
     /**
-     * Распоряжается контроллеру удалить пост с указанным номером и,
-     * если этот процесс не будет прерван исключением, записывает в ответ "OK"
-     * в стиле plain text.
-     * @param id       номер поста, который требуется удалить.
-     * @param response ответ, в который будет записано "OK".
-     * @throws IOException при проблемах со связью.
+     * Распоряжается контроллеру удалить пост с указанным номером.
      */
-    public void removeById(long id, HttpServletResponse response) throws IOException {
-        response.setContentType(TEXT_PLAIN);
+    @DeleteMapping("/{id}")
+    public void removeById(@PathVariable long id) {
         service.removeById(id);
-        response.getWriter().print("OK");
     }
 }
--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,91 +1,91 @@
-package ru.netology.servlet;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.netology.controller.PostController;
-import ru.netology.exception.NotFoundException;
-import ru.netology.repository.PostRepositoryImpl;
-import ru.netology.service.PostService;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * Осуществляет диспетчеризацию запросов.
- */
-public class MainServlet extends HttpServlet {
-    public static final String GET = "GET";
-    public static final String POST = "POST";
-    public static final String DELETE = "DELETE";
-    private PostController controller;
-
-    final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("ru.netology");
-
-    /**
-     * Инициализует слои.
-     */
-    @Override
-    public void init() {
-        final var repository = (PostRepositoryImpl) context.getBean("postRepositoryImpl");
-        final var service = (PostService) context.getBean("postService");
-        controller = (PostController) context.getBean("postController");
-    }
-
-    /**
-     * Осуществляет обработку запроса и переводит его в команду для контроллера.
-     * @param req  запрос, который обрабатывается.
-     * @param resp ответ, который будет послан.
-     */
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) {
-        // разворачивались в корневой контекст
-        try {
-            final var path = req.getRequestURI();
-            final var method = req.getMethod();
-            final var isNumbered = path.matches("/api/posts/\\d+");
-            long postId = -1;
-            if (isNumbered) {
-                try {
-                    postId = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
-                } catch (Exception ignored) {}
-            }
-
-            // получение всех постов
-            if (GET.equals(method) && "/api/posts".equals(path)) {
-                controller.all(resp);
-                return;
-            }
-
-            // получение поста по номеру
-            if (GET.equals(method) && isNumbered) {
-                if (postId <= 0)
-                    throw new NotFoundException("Невнятный номер поста.");
-                controller.getById(postId, resp );
-                return;
-            }
-
-            // публикация или обновление поста (номер в теле)
-            if (POST.equals(method) && "/api/posts".equals(path)) {
-                controller.save(req.getReader(), resp);
-                return;
-            }
-
-            // удаление поста по номеру
-            if (DELETE.equals(method) && isNumbered) {
-                if (postId <= 0)
-                    throw new NotFoundException("Невнятный номер поста.");
-                controller.removeById(postId, resp);
-                return;
-            }
-
-            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-
-        } catch (NotFoundException e) {
-            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        } catch (Exception e) {
-            e.printStackTrace();
-            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        }
-    }
-}
-
+//package ru.netology.servlet;
+//
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import ru.netology.controller.PostController;
+//import ru.netology.exception.NotFoundException;
+//import ru.netology.repository.PostRepositoryImpl;
+//import ru.netology.service.PostService;
+//
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+///**
+// * Осуществляет диспетчеризацию запросов.
+// */
+//public class MainServlet extends HttpServlet {
+//    public static final String GET = "GET";
+//    public static final String POST = "POST";
+//    public static final String DELETE = "DELETE";
+//    private PostController controller;
+//
+//    final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("ru.netology");
+//
+//    /**
+//     * Инициализует слои.
+//     */
+//    @Override
+//    public void init() {
+//        final var repository = (PostRepositoryImpl) context.getBean("postRepositoryImpl");
+//        final var service = (PostService) context.getBean("postService");
+//        controller = (PostController) context.getBean("postController");
+//    }
+//
+//    /**
+//     * Осуществляет обработку запроса и переводит его в команду для контроллера.
+//     * @param req  запрос, который обрабатывается.
+//     * @param resp ответ, который будет послан.
+//     */
+//    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+//        // разворачивались в корневой контекст
+//        try {
+//            final var path = req.getRequestURI();
+//            final var method = req.getMethod();
+//            final var isNumbered = path.matches("/api/posts/\\d+");
+//            long postId = -1;
+//            if (isNumbered) {
+//                try {
+//                    postId = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
+//                } catch (Exception ignored) {}
+//            }
+//
+//            // получение всех постов
+//            if (GET.equals(method) && "/api/posts".equals(path)) {
+//                controller.all(resp);
+//                return;
+//            }
+//
+//            // получение поста по номеру
+//            if (GET.equals(method) && isNumbered) {
+//                if (postId <= 0)
+//                    throw new NotFoundException("Невнятный номер поста.");
+//                controller.getById(postId, resp );
+//                return;
+//            }
+//
+//            // публикация или обновление поста (номер в теле)
+//            if (POST.equals(method) && "/api/posts".equals(path)) {
+//                controller.save(req.getReader(), resp);
+//                return;
+//            }
+//
+//            // удаление поста по номеру
+//            if (DELETE.equals(method) && isNumbered) {
+//                if (postId <= 0)
+//                    throw new NotFoundException("Невнятный номер поста.");
+//                controller.removeById(postId, resp);
+//                return;
+//            }
+//
+//            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+//
+//        } catch (NotFoundException e) {
+//            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//        }
+//    }
+//}
+//
